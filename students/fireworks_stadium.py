394fc8328d7049ee269213e15aca0579079aeb52
Jon Culver <jpmculver@gmail.com>
2021-12-04 19:14:16 +0000

    update to stadium.py

--- students/fireworks_stadium.py
+++ students/fireworks_stadium.py
@@ -29,3 +29,28 @@ def draw_wall(x,y,z,radius, height):
 for n in range(12):
     draw_wall(x, y, z, 25-n, 12-n)
 
+# Create Fireworks
+rocket = Firework(mc, x, z, delay=0, color="R")
+# Launch Fireworks
+while not rocket.finished():
+    rocket.move()
+    time.sleep(0.1)
+
+def display():
+    # Create Fireworks
+    rockets = []
+
+    for i in range(10):
+        gap = i * 2
+        rocket = Firework(mc, x, z + gap, delay=gap)
+        rockets.append(rocket)
+    # Launch Fireworks
+    while len(rockets) > 0:
+        for r in rockets:
+            r.move()
+            if r.finished():
+                rockets.remove(r)
+        time.sleep(0.05)
+
+for n in range(20):
+    display()
